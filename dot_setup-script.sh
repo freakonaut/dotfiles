387c90aedbe0c18c9487f0c08d3a8429c73aeaf3
freakonaut <bouliaczoo@gmail.com>
2024-02-25 16:55:11 +0100

    "beautify and removed ambiguous cd"

--- dot_setup-script.sh
+++ dot_setup-script.sh
@@ -1,32 +1,28 @@
 #!/bin/sh
-sudo pacman -S git 
+sudo pacman -S git base-devel 
 
-cd ~/
-mkdir pix vid doc mus dl 
+mkdir ~/pix ~/vid ~/doc ~/mus ~/dl ~/.local/src
 
-#Paru install
-sudo pacman -S --needed base-devel
-mkdir ~/.local/src
-cd ~/.local/src
-git clone https://aur.archlinux.org/paru.git
-cd ~/.local/src/paru
-makepkg -si
+#Git clone : Dmenu Dwm Dwmblocks St Paru
+git clone https://github.com/LukeSmithxyz/dmenu.git ~/.local/src
+git clone https://github.com/LukeSmithxyz/dwm.git ~/.local/src
+git clone https://github.com/LukeSmithxyz/dwmblocks.git ~/.local/src
+git clone https://github.com/LukeSmithxyz/st.git ~/.local/src
 
-#Git clone : Dmenu Dwm Dwmblocks St 
-cd ~/.local/src
-git clone https://github.com/LukeSmithxyz/dmenu.git
-git clone https://github.com/LukeSmithxyz/dwm.git
-git clone https://github.com/LukeSmithxyz/dwmblocks.git
-git clone https://github.com/LukeSmithxyz/st.git
+git clone https://aur.archlinux.org/paru.git ~/.local/src
+makepkg -si ~/.local/src/paru
 
 #Packages install
-paru -S aria2 imagemagick feh firefox fzf gimp gimp-plugin-bimp htop man-db mpv pamixer mlocate python-pywal python-pywalfox rsync thunar tldr xdotool xwallpaper yt-dlp ytfzf sysstat zsh zsh-syntax-highlighting ripgrep ttf-jetbrains-mono-nerd github-cli
+paru -S aria2 imagemagick feh firefox fzf gimp gimp-plugin-bimp htop \
+man-db mpv pamixer mlocate python-pywal python-pywalfox rsync thunar \
+tldr xdotool xwallpaper yt-dlp ytfzf sysstat zsh                     \
+zsh-syntax-highlighting ripgrep ttf-jetbrains-mono-nerd github-cli   \
+bob   
 
 #Ohmyzsh install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #NvChad install
-paru -S bob 
 bob use 0.9.4 
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
